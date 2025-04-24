bfe16c54526c194708e651334aabc4188f535585
EveHackmon <157503186+EveHackmon@users.noreply.github.com>
2025-04-24 17:38:18 +0300

    update4

--- Backend/Product-Service/src/services/productsService.js
+++ Backend/Product-Service/src/services/productsService.js
@@ -1,15 +1,15 @@
 // products-service/src/services/productsService.js
 
-import { Product } from '../data-access/productsModel.js';
+import { Products } from '../data-access/productsModel.js';
 
 export const productsService = {
   async fetchAllProducts() {
     try {
       const products = await Products.findAll();
       return products;
-    } catch (err) {
-      console.error('Error fetching products:', err);
-      throw new Error('Failed to fetch products');
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      throw error;
     }
   }, 
   async fetchProduct(input) {
