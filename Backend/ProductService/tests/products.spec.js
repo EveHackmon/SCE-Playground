6d0679e0764e79fde25542c402ffa9c7539efead
DanielHouri5 <hdanielh50@gmail.com>
2025-06-04 18:20:22 +0300

    aaa

--- Backend/ProductService/tests/products.spec.js
+++ Backend/ProductService/tests/products.spec.js
@@ -11,10 +11,8 @@ chai.use(chaiHttp);
 let server = null;
 
 describe('Product Service Tests', () => {
-  const productId = 69;
-
   before(async function () {
-    this.timeout(10000);
+    this.timeout(100000);
     server = await startTestServer();
     console.log('Product tests is running');
   });
@@ -23,14 +21,60 @@ describe('Product Service Tests', () => {
     server.close();
   });
 
+  it('should update existing product', (done) => {
+    chai.request
+      .execute(app)
+      .put(`/update-product/${69}`)
+      .send({ name: 'Updated Name', price: 777 })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.name).to.equal('Updated Name');
+        expect(parseFloat(res.body.price)).to.equal(777);
+        done();
+      });
+  });
+
   it('should read a product by ID', (done) => {
     chai.request
       .execute(app)
-      .get(`/read-product/${productId}`)
+      .get(`/read-product/${69}`)
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.body).to.have.property('id').equal(productId);
-        expect(res.body).to.have.property('name').equal('Mesos');
+        expect(res.body).to.have.property('id').equal(69);
+        expect(res.body).to.have.property('name').equal('Updated Name');
+        done();
+      });
+  });
+
+  it('should get all products', (done) => {
+    chai.request
+      .execute(app)
+      .get('/read-all-products')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        done();
+      });
+  });
+
+  it('should get all leads', (done) => {
+    chai.request
+      .execute(app)
+      .get('/read-all-leads')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        done();
+      });
+  });
+
+  it('should delete the created product', (done) => {
+    chai.request
+      .execute(app)
+      .delete(`/delete-product/${69}`)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equal('Product deleted successfully');
         done();
       });
   });
